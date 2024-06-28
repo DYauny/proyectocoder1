6d82508c46cb8b8473357d71bc9b668d05e2526d
Daniel Yauny <danielyauny@gmail.com>
2024-06-28 18:55:22 -0300

    Ingreso la funcion multiplicar y dividir

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,3 +7,8 @@ def sumar( n1, n2):
     print("El resultsdo de la suma entre n1 + n2 es de: ", n1 + n2)
     
     sumar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
+    
+def multiplicar( n1, n2):
+    print("El resultsdo de la multiplicacion entre n1 * n2 es de: ", n1 * n2)
+    
+    multiplicar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -2,3 +2,8 @@ def restar( n1, n2):
     print("El resultsdo de la resta entre n1 - n2 es de: ", n1 - n2)
     
     restar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
+    
+def dividir( n1, n2):
+    print("El resultsdo de la divicion entre n1 / n2 es de: ", n1 / n2)
+    
+    dividir(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
