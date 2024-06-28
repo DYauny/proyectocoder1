6a0b965136d32009424a4d36dfe615207f054696
Daniel Yauny <danielyauny@gmail.com>
2024-06-28 18:23:00 -0300

    Agrego la funcion sunar en el ejemplo1 y la funcion restar en el archivo ejemolio2

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,3 +2,8 @@ def mensaje():
     print("Estoy trabajando con git")
     
     mensaje()
+    
+def sumar( n1, n2):
+    print("El resultsdo de la suma entre n1 + n2 es de: ", n1 + n2)
+    
+    sumar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
