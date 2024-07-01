ab8b387ba4819751be783894ea0357adb19f0887
Daniel Yauny <danielyauny@gmail.com>
2024-07-01 19:49:48 -0300

    Cree la rana dan

--- ejemplo1.py
+++ ejemplo1.py
@@ -14,3 +14,6 @@ def multiplicar( n1, n2):
     print("El resultsdo de la multiplicacion entre n1 * n2 es de: ", n1 * n2)
     
     multiplicar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
+    
+def mensaje2():
+    print("Cree la rama dan")
